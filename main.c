86c1a5309e15324a3ca38c7a5fcc40910fabe795
Florent VIOLIN <fviolin@e1r5p23.42.fr>
2016-03-12 17:13:16 +0100

    backup

--- main.c
+++ main.c
@@ -6,10 +6,45 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/29 10:34:00 by fviolin           #+#    #+#             */
-/*   Updated: 2016/02/29 10:36:01 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/03/12 16:40:17 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	prompt()
+{
+	write(1, "$> ", 3);
+}
 
+
+
+int		main(int ac, char **av, char **env)
+{
+	char	**cpy_env;
+	char	*line;
+//	pid_t 	pid;
+
+//	pid = fork();
+	cpy_env = NULL;
+	av = NULL;
+	env = NULL;
+	if (ac == 1)
+	{
+		while (1)
+		{
+			prompt();
+			if (get_next_line(0, &line) == 1)
+			{
+				ft_putendl("Test Prompt");
+				printf("line in Gnl -> |%s|\n", line);
+			}
+			if (ft_strcmp(line, "exit") == 0)
+			{
+				ft_putendl("Test Exit");
+				break ;
+			}
+		}
+	}
+	return (0);
+}
--- minishell.h
+++ minishell.h
@@ -6,11 +6,18 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/29 10:21:19 by fviolin           #+#    #+#             */
-/*   Updated: 2016/02/29 10:22:13 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/03/12 15:16:28 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
 
-#include <stdio.h> //// ---TEST---
+#include "libft/libft.h"
+
+#endif
