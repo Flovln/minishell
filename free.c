b340602d0cd154dc617ee9edbeff98b9e5906ae5
Florent VIOLIN <fviolin@student.42.fr>
2016-03-17 16:58:21 +0100

    new file including free functions

---
+++ free.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/17 16:05:53 by fviolin           #+#    #+#             */
+/*   Updated: 2016/03/17 16:16:21 by fviolin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+/*
+void	free_ptrs(char **env_cpy, char **cmd, char **path_cpy)
+{
+	ft_strdel(env_cpy);
+	ft_strdel(path_cpy);
+	ft_strdel(cmd);
+}
+*/
+void	free_fork(char **cmd_tmp, char **path, char *cmd_path, char *path_str)
+{
+	free_tab(cmd_tmp);
+	free_tab(path);
+	ft_strdel(&cmd_path);
+	ft_strdel(&path_str);
+}
+
+void	free_tab(char **tab)
+{
+	int i;
+	int len;
+
+	if (tab)
+	{
+		if (*tab)
+		{
+			i = 0;
+			len = ft_tablen(tab);
+			while (i < len)
+			{
+				ft_strdel(&tab[i]);
+				i++;
+			}
+		}
+		free(tab);
+		tab = NULL;
+	}
+}
